6b82d7d3900e53471c88fdea953d1ff1641669a3
giovandrofabio <giovandrofabiosantos@hotmail.com>
2022-05-22 22:43:41 -0300

    Create Route User,Supermercat

---
+++ Backend/src/controllers/controller.usuarios.js
@@ -0,0 +1,101 @@
+import { Route } from "express";
+import db from '../config/database.js';
+
+const controllerUsuarios = Route();
+
+controllerUsuarios.post("/usuarios/login", function(request, response){
+    
+    let ssql = "select id_usuario, nome, email, endereco, bairro, uf, cep, "
+    ssql += "date_format(dt_cadastro, '%d/%m/%Y %H:%i:%s') as dt_cadastro "
+    ssql += " from usuario where email=? and senha=?"
+
+    db.query(ssql, [request.body.email, request.body.senha], function(err, result){
+        if (err){
+            return response.status(500).send(err);
+        }else{
+            return response.status(result.length > 0 ? 200 : 401).json(result[0]);
+        }
+    })    
+});
+
+controllerUsuarios.post("/usuarios/cadastro", function(request, response){
+    let body = request.body 
+    let ssql = "INSERT INTO usuarios (nome, email, senha, endereco, bairro, cidade, uf, cep, dt_cadastro) "
+    ssql += " VALUES(?,?,?,?,?,?,?,?,current_timestamp()) "
+ 
+    db.query(ssql, [body.nome,
+                    body.email,
+                    body.senha,
+                    body.endereco,
+                    body.bairro,
+                    body.cidade,
+                    body.uf,
+                    body.cep], function(err, result){
+        if (err){
+            return response.status(500).send(err);
+        }else{
+            return response.status(201).json({id_usuario: result.insertId});
+        }
+    })    
+});
+
+controllerUsuarios.get("/usuarios/:id_usuario", function(request, response){
+
+    let ssql = "select * from usuario where id_usuario = ?"
+    db.query(ssql, [request.params.id_usuario], function(err, result){
+        if (err){
+            return response.status(500).send(err);
+        }else{
+            return response.status(result.length > 0 ? 200 : 404).json(result[0]);
+        }
+    })    
+});
+
+controllerUsuarios.put("/usuarios/:id_usuario", function(request, response){
+    let body = request.body 
+    let ssql = "UPDATE usuarios SET nome=?, email=?, senha=?, endereco=?, bairro=?, cidade=?, uf=?, cep=? "
+    ssql += " WHERE id_usuario = ? "
+ 
+    db.query(ssql, [body.nome,
+                    body.email,
+                    body.senha,
+                    body.endereco,
+                    body.bairro,
+                    body.cidade,
+                    body.uf,
+                    body.cep,
+                    request.params.id,], function(err, result){
+        if (err){
+            return response.status(500).send(err);
+        }else{
+            return response.status(200).json({id_usuario: request.params.id});
+        }
+    })    
+});
+
+controllerUsuarios.get("/usuarios", function(request, response){
+
+    let ssql = "select * from usuario"
+    db.query(ssql, function(err, result){
+        if (err){
+            return response.status(500).send(err);
+        }else{
+            return response.status(200).json(result);
+        }
+    })    
+});
+
+controllerUsuarios.delete("/usuarios/:id", function(request, response){
+    let body = request.body 
+    let ssql = "DELETE FROM usuarios WHERE id_usuario = ? "
+ 
+    db.query(ssql, [request.params.id,], function(err, result){
+        if (err){
+            return response.status(500).send(err);
+        }else{
+            return response.status(result.length > 0 ? 200 : 401).json(result[0]);
+        }
+    })    
+});
+
+export default controllerUsuarios;
--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,9 +1,20 @@
-import express from 'express';
+import express from "express";
+import cors from "cors";
+import db from "./config/database.js";
+//import controllerMercados from "./controllers/controller.mercados.js";
+//import {controllerUsuarios} from "./controllers/controller.usuarios.js";
 
 const app = express();
 
 //Middleware JSON
 app.use(express.json());
+
+//Middleware CORS
+app.use(cors());
+
+//Controllers
+//app.use(controllerUsuarios);
+//app.use(controllerMercados);
 
 /*
     Verbos HTTP:
@@ -13,34 +24,131 @@ app.use(express.json());
     PUT -> Editar dados
     DELETE -> Excluir dados
 */
-
 //Query Params...
-app.get("/clientes", function(request, response){
-    return response.send("Listando todos os clientes ordenados por: " + request.query.ordem + 
-    " listando e apenas " + request.query.top + " registros");    
+app.get("/usuarios", function(request, response){
+
+    let ssql = "select * from usuario"
+    db.query(ssql, function(err, result){
+        if (err){
+            return response.status(500).send(err);
+        }else{
+            return response.status(200).json(result);
+        }
+    })    
 });
 
 //URI Params...
-app.get("/clientes/:id", function(request, response){
-    return response.send("Listando somente o cliente: " + request.params.id);    
+app.get("/usuarios/:id_usuario", function(request, response){
+
+    let ssql = "select * from usuario where id_usuario = ?"
+    db.query(ssql, [request.params.id_usuario], function(err, result){
+        if (err){
+            return response.status(500).send(err);
+        }else{
+            return response.status(result.length > 0 ? 200 : 404).json(result[0]);
+        }
+    })    
 });
 
-app.post("/clientes", function(request, response){
-    const body = request.body
-    return response.send("Novo cliente: " + body.nome + " - " +
-    body.email);    
+app.post("/usuarios/login", function(request, response){
+    
+    let ssql = "select id_usuario, nome, email, endereco, bairro, uf, cep, "
+    ssql += "date_format(dt_cadastro, '%d/%m/%Y %H:%i:%s') as dt_cadastro "
+    ssql += " from usuario where email=? and senha=?"
+
+    db.query(ssql, [request.body.email, request.body.senha], function(err, result){
+        if (err){
+            return response.status(500).send(err);
+        }else{
+            return response.status(result.length > 0 ? 200 : 401).json(result[0]);
+        }
+    })    
 });
 
-app.put("/clientes", function(request, response){
-    return response.send("Alterando um cliente com PUT");    
+app.post("/usuarios/cadastro", function(request, response){
+    let body = request.body 
+    let ssql = "INSERT INTO usuario (nome, email, senha, endereco, bairro, cidade, uf, cep, dt_cadastro) "
+    ssql += " VALUES(?,?,?,?,?,?,?,?,current_timestamp()) "
+ 
+    db.query(ssql, [body.nome,
+                    body.email,
+                    body.senha,
+                    body.endereco,
+                    body.bairro,
+                    body.cidade,
+                    body.uf,
+                    body.cep], function(err, result){
+        if (err){
+            return response.status(500).send(err);
+        }else{
+            return response.status(201).json({id_usuario: result.insertId});
+        }
+    })    
 });
 
-app.patch("/clientes", function(request, response){
-    return response.send("Alterando um cliente com PATCH");    
+app.put("/usuarios/:id_usuario", function(request, response){
+    let body = request.body 
+    let ssql = "UPDATE usuario SET nome=?, email=?, senha=?, endereco=?, bairro=?, cidade=?, uf=?, cep=? "
+    ssql += " WHERE id_usuario = ? "
+ 
+    db.query(ssql, [body.nome,
+                    body.email,
+                    body.senha,
+                    body.endereco,
+                    body.bairro,
+                    body.cidade,
+                    body.uf,
+                    body.cep,
+                    request.params.id_usuario,], function(err, result){
+        if (err){
+            return response.status(500).send(err);
+        }else{
+            return response.status(200).json({id_usuario: request.params.id_usuario});
+        }
+    })    
 });
 
-app.delete("/clientes", function(request, response){
-    return response.send("Excluindo Clientes");    
+app.delete("/usuarios/:id", function(request, response){
+    let body = request.body 
+    let ssql = "DELETE FROM usuarios WHERE id_usuario = ? "
+ 
+    db.query(ssql, [request.params.id,], function(err, result){
+        if (err){
+            return response.status(500).send(err);
+        }else{
+            return response.status(result.length > 0 ? 200 : 401).json(result[0]);
+        }
+    })    
+});
+
+app.get("/mercados", function(request, response){
+
+    let filtro = [];
+    let ssql = "select * from mercado ";
+    ssql += "where id_mercado >0";
+
+    if(request.query.busca){
+        ssql += "and nome =? ";
+        filtro.push(request.query.busca);    
+    }
+
+    if(request.query.ind_entrega){
+        ssql += "and ind_entrega =? ";
+        filtro.push(request.query.ind_entrega);    
+    } 
+
+    if(request.query.ind_retira){
+        ssql += "and ind_retira =? ";
+        filtro.push(request.query.ind_retira);    
+    }     
+
+    db.query(ssql, filtro,function(err, result){
+        if (err){
+            return response.status(500).send(err);
+        }else{
+            return response.status(200).json(result);
+        }
+    })    
 });
 
 app.listen(3000, function(){
